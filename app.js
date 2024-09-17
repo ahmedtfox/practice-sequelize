b00ae3efe3168f86c93fd9d17c9197684537440d
AhmedMahfouz <ahmedmahfouzt@gmail.com>
2024-09-17 17:06:04 +0300

    Models

--- app.js
+++ app.js
@@ -3,12 +3,54 @@ const db = new Sequelize("sequelize", "root", "ahmednodemysql", {
   host: "localhost",
   port: 3306,
   dialect: "mysql",
+
+  define: {
+    freezeTableName: true,
+    timestamps: false,
+  },
+
 });
 
-db.authenticate()
-  .then(() => {
-    console.log("Connection has been established successfully.");
+const User = db.define(
+  "user",
+  {
+    user_id: {
+      type: Sequelize.DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: Sequelize.DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: Sequelize.DataTypes.STRING,
+    },
+    age: {
+      type: Sequelize.DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 21,
+    },
+    num: {
+      type: Sequelize.DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 21,
+    },
+  },
+  {
+    freezeTableName: true,
+    timestamps: false,
+  }
+);
+/* 
+db.sync({ alter: true })
+  .then((data) => {
+    console.log("done");
   })
   .catch((err) => {
-    console.error("Unable to connect to the database:", err);
+    console.log(err);
   });
+ */
+//db.drop({ match: /_test$/ });
+
+console.log(db.models.user);
